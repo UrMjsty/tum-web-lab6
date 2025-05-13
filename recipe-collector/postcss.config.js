8bce65c54f32d662a72b7633117d77221619ff44
UrMjsty <alexciornyi@gmail.com>
2025-05-13 20:10:15 +0300

    add tailwind postcss

---
+++ recipe-collector/postcss.config.js
@@ -0,0 +1,10 @@
+// ✅ NEW for Tailwind v4+
+import tailwindcss from '@tailwindcss/postcss';
+import autoprefixer from 'autoprefixer';
+
+export default {
+    plugins: [
+        tailwindcss(),
+        autoprefixer(),
+    ],
+};
